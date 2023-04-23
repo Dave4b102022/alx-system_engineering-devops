0d60056263171d75a61d768f077c0df28a647c70
Tsaku David Yakubu <dave4b10@gmail.com>
2023-04-23 16:47:53 -0700

    tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,14 @@
-# set up your client SSH configuration file so that you can
-# connect to a server without typing a password
+#!/usr/bin/env bash
+# Puppet manifest to configure ssh client with public key authentication
 
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
+file_line { 'Turn off password authentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
